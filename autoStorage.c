ae970d6f41ed45e1d08fcb9bd746f08d9d61df20
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-05-06 01:45:45 +0530

    Add files via upload
    In C Today's Topic: Strings & Storage Classes in C
    
    Learn how to create and access strings in C, and understand the concept of storage classes (auto, register, static, extern).oday learn about how to createa string and Access it and learn the concept of storage classes

---
+++ autoStorage.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main ()
+{
+    // this are local variables if we not specify the auto keyword but also the variable is auto
+    int i ;
+    auto int j ;      // auto storage class variable 
+    int k ;
+
+    return 0;
+}
